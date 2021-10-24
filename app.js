59ba5c66f893c72296cdacba02f757328ff76641
Tamago <58776029+Yuko-code@users.noreply.github.com>
2021-10-24 22:27:13 +1100

    Trivia API
    App to get trivia questions!

---
+++ app.js
@@ -0,0 +1,109 @@
+
+const btnGet=document.getElementById("btnGet");
+let choices=""
+let answer="";
+
+const genres=[
+    {
+        name:"General Knowledge",
+        id:9
+    },
+    {
+        name:"History",
+        id:23
+    },
+    {
+        name:"Music",
+        id:12
+    },
+    {
+        name:"Video Games",
+        id:15
+    },
+    {
+        name:"Fillm",
+        id:11
+    },
+    {
+        name:"Science/Nature",
+        id:17
+    },
+    {
+        name:"Science/Computers",
+        id:18
+    },
+    {
+        name:"Sports",
+        id:21
+    },
+    {
+        name:"Geography",
+        id:22
+    },
+    {
+        name:"Cartoon&Animations",
+        id:32
+    },
+    {
+        name:"Celebrities",
+        id:26
+    }
+]
+
+btnGet.addEventListener('click', function(){
+    const genre=document.getElementById('genre').selectedIndex;
+    const selectedId=genres[genre].id
+    document.getElementById("choices").innerHTML="<button id='btnChoices'>Display Choices</button>"
+    document.getElementById("answer").innerHTML="<button id='btnDisplayAnswer'>Display Aswer</button>"
+    fetch(`https://opentdb.com/api.php?amount=1&category=${selectedId}&type=multiple`)
+    .then(
+        function(response){
+            if(response.status!==200){
+                console.log('Looks like there was a problem. Status Code: ' + response.status);
+                return;
+            }
+
+            response.json().then(function(data){
+                //console.log(data.results[0])
+                answer=data.results[0].correct_answer;
+                const question=data.results[0].question;
+                const category=data.results[0].category;
+                document.getElementById("category").innerHTML=category;
+                document.getElementById("question").innerHTML=question;
+
+                document.getElementById("btnChoices").addEventListener('click',function(){
+                btnChoices.setAttribute('disabled','disabled')
+                const choiceCorrect=data.results[0].correct_answer;
+                const choicesIncorrect=data.results[0].incorrect_answers;
+                const arr=choicesIncorrect;
+                arr.push(choiceCorrect)
+                const newArr=[...arr];
+
+                let currentIndex=newArr.length;
+                while(currentIndex !=0){
+                    randomIndex=Math.floor(Math.random()*currentIndex);
+                    currentIndex--;
+                    [newArr[currentIndex], newArr[randomIndex]] = [
+                        newArr[randomIndex], newArr[currentIndex]];
+                }
+                for(let i=0; i<arr.length; i++){  
+                    const list=document.createElement('li')
+                    document.getElementById("choices").appendChild(list).innerHTML=newArr[i]
+                } 
+                })
+
+                document.getElementById("btnDisplayAnswer").addEventListener('click',function(){
+                    document.getElementById("answer").innerHTML=answer;
+                });
+            });
+        }
+    )
+    .catch(function(err){
+        console.log('Fetch Error :-S', err);
+    });
+});
+
+
+
+
+
